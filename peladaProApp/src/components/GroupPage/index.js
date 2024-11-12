d6301f8896ffee9f54029fbf8ea8f5e005d6c442
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2024-11-12 18:10:27 -0300

    Profile page

--- peladaProApp/src/components/GroupPage/index.js
+++ peladaProApp/src/components/GroupPage/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
+import styles from "./styles";
 
 export default function GroupPage(){
     return(
-        <View>
-            <Text>Grupos</Text>
+        <View style={styles.mainView}>
+            <View style={styles.search}>
+                <Text style={styles.text}>Pesquisar Grupos:</Text>
+                <TextInput placeholder='Nome do grupo:' style={styles.searchBar}/>
+            </View>
+            <View style={styles.myGroups}>
+                <Text style={styles.text}>Meus Grupos</Text>
+                <View style={styles.groups}>
+                </View>
+            </View>
         </View>
     )
 }
---
+++ peladaProApp/src/components/GroupPage/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        width: '100%'
+    },
+    search: {
+        width: '100%',
+        height: 'auto',
+        paddingLeft: 20,
+        paddingTop: 15,
+        paddingRight: 20,
+    },
+    text: {
+        color: '#167830',
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    searchBar: {
+        backgroundColor: '#ababab',
+        paddingHorizontal: 15,
+        paddingVertical: 5
+    },
+    myGroups: {
+        width: '100%',
+        height: 'auto',
+        paddingLeft: 20,
+        paddingTop: 15,
+        paddingRight: 20,
+    },
+    groups: {
+        
+    }
+})
+
+export default styles
